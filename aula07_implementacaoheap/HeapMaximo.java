ce4905450582f73622e642982afb30511e4af9aa
leonardoheredia <leonardo@heredia.com.br>
2024-03-18 20:20:31 -0300

    Aula 07 - Implementacao

--- aula07_implementacaoheap/HeapMaximo.java
+++ aula07_implementacaoheap/HeapMaximo.java
@@ -22,6 +22,32 @@ public class HeapMaximo {
             nadarSubindo(pai);
         }
     }
+    public int extrairMaximo() {
+        int max = chaves[0];
+        chaves[0] = chaves[tamanho-1];
+        chaves[tamanho-1] = max;
+        tamanho--;
+        afundar(0);
+        return max;
+    }
+    public void afundar(int posicao) {
+        int maior = posicao;
+        int fe = 2*posicao + 1;
+        int fd = 2*posicao + 2;
+        if(fe < tamanho && chaves[fe] > chaves[maior]) {
+            maior = fe;
+        }
+        if(fd < tamanho && chaves[fd] > chaves[maior]){
+            maior = fd;
+        }
+        if(maior!=posicao) {
+            int temp = chaves[posicao];
+            chaves[posicao] = chaves[maior];
+            chaves[maior] = temp;
+            afundar(maior);
+        }
+    }
+
 
     public static void main(String[] args) {
         HeapMaximo h = new HeapMaximo(50);
@@ -30,5 +56,6 @@ public class HeapMaximo {
         h.inserir(80);
         h.inserir(23);
         h.inserir(90);
+        System.out.println(h.extrairMaximo());
     }
 }
